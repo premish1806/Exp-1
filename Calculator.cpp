8cc6cbffdd79bd55e8e80646c6c383815a9eb5f2
premish1806 <premishninawe@gmail.com>
2024-08-11 12:11:36 +0530

    Add files via upload

---
+++ Calculator.cpp
@@ -0,0 +1,16 @@
+// Name - Premish Ninawe
+// PRN - 23070123092
+#include <iostream>
+using namespace std;
+int main() {
+    float num1, num2;
+    cout << "Please enter the first number: ";
+    cin >> num1;
+    cout << "Please enter the second number: ";
+    cin >> num2;
+    cout << "The sum of " << num1 << " and " << num2 << " is: " << num1 + num2 << "\n";
+    cout << "The difference when subtracting " << num2 << " from " << num1 << " is: " << num1 - num2 << "\n";
+    cout << "The result of dividing " << num1 << " by " << num2 << " is: " << num1 / num2 << "\n";
+    cout << "The product of " << num1 << " and " << num2 << " is: " << num1 * num2 << "\n";
+    return 0;
+}
